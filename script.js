5693d33debd3aa5fb147497c06a6614afea10ea9
chinonso01 <irobindachinonso@gmail.com>
2022-10-03 14:22:44 -0700

    adding some file

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload(init);
+
+function init () {
+
+    var list = ["goat", "rat", "hen"];
+
+    var check = true;
+
+    for(var i = 0; i = list.length; i ++){
+        if(check){
+            console.log("you've do it")
+        }
+    }
+}
